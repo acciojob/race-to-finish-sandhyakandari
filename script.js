a18af0b6410e69d4216f78c63f8caa3e94c9f480
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-07-06 12:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 window.promises = [];
-
+let p1=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("resolve after 1 sec");},1000);
+})
+let p2=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("resolve after 2 sec");},2000);
+})
+let p3=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("resolve after 3 sec");},3000);
+})
+let p4=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("resolve after 4 sec");},4000);
+})
+let p5=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("resolve after 5 sec");},5000);
+})
 // Do not change the code above this
 // add your promises to the array `promises`
+promises.push(p1,p2,p3,p4,p5);
+
+try{
+	let res=promise.any(promises);
+    res.then((data))=>{
+		let div=document.getElementById("output");
+		div.innerText=data;
+	}
+}
